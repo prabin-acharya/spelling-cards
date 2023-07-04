0b82f2c89339fdeaaa808b9cd61d236460cc62e0
Prabin Acharya <acharyaprabin101@gmail.com>
2023-07-04 19:19:59 +0545

    download as csv file

--- popup.js
+++ popup.js
@@ -8,3 +8,20 @@ chrome.storage.local.get("words", function (data) {
     wordList.appendChild(li);
   });
 });
+
+document.getElementById("download").addEventListener("click", function () {
+  chrome.storage.local.get(["words"], function (result) {
+    var words = result.words;
+    var csv = "Typed Word,Correct Spelling\n";
+    words.forEach(function (word) {
+      var parts = word.split("-");
+      csv += parts[0] + "," + parts[1] + "\n";
+    });
+    var blob = new Blob([csv], { type: "text/csv" });
+    var url = URL.createObjectURL(blob);
+    var link = document.createElement("a");
+    link.href = url;
+    link.download = "words.csv";
+    link.click();
+  });
+});
